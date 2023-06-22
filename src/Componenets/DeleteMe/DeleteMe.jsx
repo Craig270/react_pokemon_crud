86ff78b7dc0860f8978bed18d14253c516830669
Craig <Craig.Neff@tivityhealth.com>
2023-06-21 23:22:30 -0600

    updated deleteme comp

--- src/Componenets/DeleteMe/DeleteMe.jsx
+++ src/Componenets/DeleteMe/DeleteMe.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const DeleteMe = ({ name }) => {
-  return <div>Hello, {name}</div>;
+  return (
+    <div>
+      {" "}
+      <p>Hello user!</p>Hello, {name}
+    </div>
+  );
 };
 
 export default DeleteMe;
