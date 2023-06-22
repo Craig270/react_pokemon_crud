4dd28f524e472d85cf3389f66f85b087192fffa3
Craig <Craig.Neff@tivityhealth.com>
2023-06-21 23:33:50 -0600

    making this branch to push it past another

---
+++ src/Componenets/Craig/Craig.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Craig = () => {
+  return (
+    <div>
+      <p>
+        {" "}
+        This comp was created to show that I can get main's branch to move past
+        another branch by doing the fallowing steps: I created a new branch and
+        made some changes then commited those changes
+      </p>
+    </div>
+  );
+};
+
+export default Craig;
