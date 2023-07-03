82394a31d669eb05e6ef509a7b4e2afb8d6b898e
Craig <Craig.Neff@tivityhealth.com>
2023-07-02 21:31:53 -0600

    useEffect hook practice

--- src/Componenets/UseEffectPractice/UseEffectPractice.jsx
+++ src/Componenets/UseEffectPractice/UseEffectPractice.jsx
@@ -2,7 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./UseEffectPractice.scss";
 
 const UseEffectPractice = () => {
-  const [resourceType, setResourceType] = useState("post");
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+    //.then((json) => console.log(json));
+  }, [resourceType]);
 
   return (
     <>
@@ -12,6 +20,9 @@ const UseEffectPractice = () => {
         <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1 className="firstClass">{resourceType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </>
   );
 };
